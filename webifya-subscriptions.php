6fa2dbcb8552b2cac7a98fcd07eb636ef5d836bd
webifya <161525559+webifya@users.noreply.github.com>
2026-07-29 02:04:09 +0600

    Rename plugin to My Subscriptions for WooCommerce

--- webifya-subscriptions.php
+++ webifya-subscriptions.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: Subscriptions for WooCommerce
+ * Plugin Name: My Subscriptions for WooCommerce
  * Description: Sell subscription products with gateway-neutral, customer-paid renewal orders.
- * Version: 0.3.1
+ * Version: 0.3.2
  * Author: Webifya
  * Text Domain: webifya-subscriptions
  * Requires at least: 6.4
@@ -15,7 +15,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'WFS_VERSION', '0.3.1' );
+define( 'WFS_VERSION', '0.3.2' );
 define( 'WFS_PLUGIN_FILE', __FILE__ );
 define( 'WFS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 
