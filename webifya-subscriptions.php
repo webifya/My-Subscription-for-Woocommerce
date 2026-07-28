19a20010835e3e6f9c6fe21392984f47c0996201
webifya <161525559+webifya@users.noreply.github.com>
2026-07-29 01:50:28 +0600

    Initialize Webifya Subscriptions 0.3.0

---
+++ webifya-subscriptions.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: Webifya Subscriptions for WooCommerce
+ * Description: Sell subscription products with gateway-neutral, customer-paid renewal orders.
+ * Version: 0.3.0
+ * Author: Webifya
+ * Text Domain: webifya-subscriptions
+ * Requires at least: 6.4
+ * Requires PHP: 7.4
+ * WC requires at least: 8.5
+ * WC tested up to: 9.9
+ *
+ * @package Webifya_Subscriptions
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+define( 'WFS_VERSION', '0.3.0' );
+define( 'WFS_PLUGIN_FILE', __FILE__ );
+define( 'WFS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+
+add_action(
+	'before_woocommerce_init',
+	static function () {
+		if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+			\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+		}
+	}
+);
+
+require_once WFS_PLUGIN_PATH . 'includes/class-wfs-plugin.php';
+
+register_activation_hook( __FILE__, array( 'WFS_Plugin', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'WFS_Plugin', 'deactivate' ) );
+
+add_action( 'plugins_loaded', array( 'WFS_Plugin', 'instance' ) );
